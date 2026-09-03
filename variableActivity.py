ca26e83b6b155bfb6b7a9db171dc59bddf5d6231
braheem45 <bwilliams845@boyslatin.net>
2026-09-03 12:51:31 +0000

    Naming Variables

---
+++ variableActivity.py
@@ -0,0 +1,28 @@
+# Variable- A contanier that stores data , you can name your container almost anything you want , as long as its descriptive.
+
+name = "braheem williams"
+
+# When we want to create a variable , we make up a word.
+# use the equal sign and then assign it a data type
+# whenever the computer sees our variable word it will always use the data inside.
+apple count - interger
+math number students
+travel insurense 
+gym membership
+
+ # Variable naming rules 
+# 1. A variable must start with a letter and cannot start with a number
+# 2. A variable can be a mix of latters and numbers but not symbols
+# 3. A variable cannot be apython keyword
+
+# Variable naming coventions
+# we use naming conventions because we cannot have spaces in vvariable names 
+# Camel case - when creating variable names with multiple words , every new word begins with a capital letter
+# the reasom why its called camel case is bc every new word represents a hump
+
+# Snnake case - when creating a variable using snake case we underscore symbol to space out each new word
+# the reason why its called snake case is bc the underscore represents a snake on the ground
+
+# Pascal case - pascal case is the same as camel case execpt
+# all words start with a capital letter 
+# its called pascal case bc its the same way we write first/ last names
